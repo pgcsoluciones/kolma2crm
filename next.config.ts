08f87512a9a379b3e791f2ad75f18bc9c8edf5e4
pgcsoluciones <fliaprince@gmail.com>
2026-02-15 05:54:53 +0000

    Disable ESLint during build for V.Piloto

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: false,
+  },
 };
 
 export default nextConfig;
